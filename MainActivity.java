d549bbf4c0b1a37677dfd70ce1850b2841d4cabd
NVM <162157911+HAJTIDev@users.noreply.github.com>
2025-10-22 11:16:54 +0200

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,22 +1,21 @@
-1. Początki smartfonów
 Kiedy i dlaczego pojawiły się pierwsze smartfony?
-Pierwsze smartfony pojawiły się na początku lat 90. XX wieku. Za pierwszy uznaje się IBM Simon Personal Communicator z 1994 roku. Powstał, ponieważ użytkownicy potrzebowali urządzenia łączącego funkcje telefonu z możliwościami komputera — do pracy, organizacji czasu i komunikacji elektronicznej.
-Jakie funkcje odróżniały smartfony od wcześniejszych telefonów komórkowych?
+Pierwsze smartfony pojawiły się na początku lat 90. XX wieku. Za pierwszy uważa się IBM Simon Personal Communicator z 1994 roku. Powstał on z potrzeby stworzenia urządzenia, które łączyłoby funkcje telefonu i komputera – do obsługi e-maili, zarządzania czasem i pracy.
+Jakie cechy odróżniały smartfony od wcześniejszych telefonów komórkowych?
 Smartfony miały:
 ekran dotykowy,
 możliwość instalowania aplikacji,
-obsługę e-maili i internetu,
-kalendarz, notatnik, kalkulator,
-później także aparat fotograficzny i GPS.
-Wcześniejsze telefony służyły głównie do dzwonienia i wysyłania SMS-ów.
-Kto był pionierem w produkcji pierwszych smartfonów?
+obsługę internetu i poczty e-mail,
+kalkulator, notatnik i kalendarz,
+a później także aparat fotograficzny i GPS.
+Wcześniejsze telefony służyły głównie do dzwonienia i wysyłania wiadomości tekstowych.
+Kto był pionierem w tworzeniu smartfonów?
 Pionierami byli:
-IBM – twórca IBM Simon (1994),
-Nokia – z modelem Nokia 9000 Communicator (1996),
-później BlackBerry, Palm, a od 2007 roku Apple z iPhone’em.
+IBM – twórca IBM Simon w 1994 roku,
+Nokia – z modelem Nokia 9000 Communicator z 1996 roku,
+a później Palm i BlackBerry, po których w 2007 roku pojawiło się Apple z iPhone’em.
 Jakie problemy techniczne ograniczały ich rozwój?
 krótki czas pracy baterii,
-duże rozmiary i waga,
+duży rozmiar i waga,
 mała pojemność pamięci,
 wolne procesory,
 ograniczony dostęp do internetu (wolne sieci 2G),
