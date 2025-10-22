9ea6418d0c59de1d3230c36cf9b2ad56a3a73c70
NVM <162157911+HAJTIDev@users.noreply.github.com>
2025-10-22 11:05:34 +0200

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,1 +1,23 @@
-Pierwsze smartfony pojawiły się w latach 90. XX wieku (np. IBM Simon) jako połączenie telefonu i komputera, wyróżniały się ekranem dotykowym, aplikacjami i dostępem do internetu, ich pionierami byli m.in. IBM i Nokia, a rozwój ograniczały problemy techniczne takie jak słaba bateria, mała pamięć i duże rozmiary.
+1. Początki smartfonów
+Kiedy i dlaczego pojawiły się pierwsze smartfony?
+Pierwsze smartfony pojawiły się na początku lat 90. XX wieku. Za pierwszy uznaje się IBM Simon Personal Communicator z 1994 roku. Powstał, ponieważ użytkownicy potrzebowali urządzenia łączącego funkcje telefonu z możliwościami komputera — do pracy, organizacji czasu i komunikacji elektronicznej.
+Jakie funkcje odróżniały smartfony od wcześniejszych telefonów komórkowych?
+Smartfony miały:
+ekran dotykowy,
+możliwość instalowania aplikacji,
+obsługę e-maili i internetu,
+kalendarz, notatnik, kalkulator,
+później także aparat fotograficzny i GPS.
+Wcześniejsze telefony służyły głównie do dzwonienia i wysyłania SMS-ów.
+Kto był pionierem w produkcji pierwszych smartfonów?
+Pionierami byli:
+IBM – twórca IBM Simon (1994),
+Nokia – z modelem Nokia 9000 Communicator (1996),
+później BlackBerry, Palm, a od 2007 roku Apple z iPhone’em.
+Jakie problemy techniczne ograniczały ich rozwój?
+krótki czas pracy baterii,
+duże rozmiary i waga,
+mała pojemność pamięci,
+wolne procesory,
+ograniczony dostęp do internetu (wolne sieci 2G),
+wysokie koszty produkcji i mała dostępność ekranów dotykowych.
