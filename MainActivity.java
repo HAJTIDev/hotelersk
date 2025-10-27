b991adb681fe3cccfc07a3ad8d247bff333ebc39
NVM <162157911+HAJTIDev@users.noreply.github.com>
2025-10-27 10:12:05 +0100

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -5,23 +5,21 @@ int main() {
     int n;
     cin >> n;
 
-    int liczby[n];
-    for (int i = 0; i < n; i++)
-        cin >> liczby[i];
+    int liczba;
+    int podzielne2 = 0, podzielne3 = 0, podzielne4 = 0, podzielne5 = 0;
 
-    int dzielniki[] = {2, 3, 4, 5};
-    int wyniki[4] = {0};
-
-    for (int x : liczby) {
-        for (int i = 0; i < 4; i++) {
-            if (x % dzielniki[i] == 0)
-                wyniki[i]++;
-        }
+    for (int i = 0; i < n; i++) {
+        cin >> liczba;
+        if (liczba % 2 == 0) podzielne2++;
+        if (liczba % 3 == 0) podzielne3++;
+        if (liczba % 4 == 0) podzielne4++;
+        if (liczba % 5 == 0) podzielne5++;
     }
 
-    for (int i = 0; i < 4; i++) {
-        cout << dzielniki[i] << ":" << wyniki[i] << endl;
-    }
+    cout << "2:" << podzielne2 << endl;
+    cout << "3:" << podzielne3 << endl;
+    cout << "4:" << podzielne4 << endl;
+    cout << "5:" << podzielne5 << endl;
 
     return 0;
 }
