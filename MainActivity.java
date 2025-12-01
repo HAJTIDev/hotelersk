6585c3ace849df0427138082f19a47ad3942f024
NVM <162157911+HAJTIDev@users.noreply.github.com>
2025-12-01 10:06:40 +0100

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,84 +1,150 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
-// Klasa bazowa
-class Zwierze {
-protected:
-    string imie;
-    int wiek;
-
+class MySort
+{
 public:
-    Zwierze(string imie, int wiek) : imie(imie), wiek(wiek) {}
+    void myBubbleSort();
+    void mySelectionSort();
+    void myInsertionSort();
+    void myBucketSort();
+    void myBogoSort();
+    void getData();
+    void getSortTimeDuration();
+    void showSortedNumbers(string name, vector<int>& tab);
 
-    void opis() {
-        cout << "Zwierze: " << imie << ", Wiek: " << wiek << endl;
-    }
+private:
+    int n = 0;
+    vector<int> tab;
+    map<string, int> sortTimeMap;
 
-    void dzwiek() {
-        cout << imie << " wydaje dzwiek." << endl;
+    bool isSorted(const vector<int>& v) {
+        for (int i = 1; i < (int)v.size(); i++) if (v[i] < v[i-1]) return false;
+        return true;
     }
 };
 
-// Klasa Ssaki
-class Ssaki : public Zwierze {
-public:
-    Ssaki(string imie, int wiek) : Zwierze(imie, wiek) {}
+void MySort::getData() {
+    cout << "Podaj liczbe elementow: ";
+    cin >> n;
+    tab.resize(n);
+    cout << "Podaj elementy: ";
+    for (int i = 0; i < n; i++) cin >> tab[i];
+}
 
-    void opis() {
-        cout << "Ssak: " << imie << ", Wiek: " << wiek << endl;
+void MySort::showSortedNumbers(string name, vector<int>& a) {
+    cout << name << ": ";
+    for (int x : a) cout << x << " ";
+    cout << "\n";
+}
+
+void MySort::getSortTimeDuration() {
+    for (auto &p : sortTimeMap) {
+        cout << p.first << " czas: " << p.second << " ms\n";
+    }
+}
+
+void MySort::myBubbleSort() {
+    vector<int> a = tab;
+    auto start = chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (a[j] > a[j+1]) swap(a[j], a[j+1]);
+        }
     }
 
-    void dzwiek() {
-        cout << imie << " mówi: Raaa!" << endl;
-    }
-};
+    auto end = chrono::high_resolution_clock::now();
+    int duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+    sortTimeMap["BubbleSort"] = duration;
+    showSortedNumbers("BubbleSort", a);
+}
 
-// Klasa Ptak
-class Ptak : public Zwierze {
-public:
-    Ptak(string imie, int wiek) : Zwierze(imie, wiek) {}
+void MySort::mySelectionSort() {
+    vector<int> a = tab;
+    auto start = chrono::high_resolution_clock::now();
 
-    void opis() {
-        cout << "Ptak: " << imie << ", Wiek: " << wiek << endl;
-    }
-
-    void dzwiek() {
-        cout << imie << " ćwierka: Tweet-tweet!" << endl;
-    }
-};
-
-// Klasa Gad
-class Gad : public Zwierze {
-public:
-    Gad(string imie, int wiek) : Zwierze(imie, wiek) {}
-
-    void opis() {
-        cout << "Gad: " << imie << ", Wiek: " << wiek << endl;
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) if (a[j] < a[minIndex]) minIndex = j;
+        swap(a[i], a[minIndex]);
     }
 
-    void dzwiek() {
-        cout << imie << " syczy: Ssss!" << endl;
+    auto end = chrono::high_resolution_clock::now();
+    int duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+    sortTimeMap["SelectionSort"] = duration;
+    showSortedNumbers("SelectionSort", a);
+}
+
+void MySort::myInsertionSort() {
+    vector<int> a = tab;
+    auto start = chrono::high_resolution_clock::now();
+
+    for (int i = 1; i < n; i++) {
+        int key = a[i];
+        int j = i - 1;
+        while (j >= 0 && a[j] > key) {
+            a[j+1] = a[j];
+            j--;
+        }
+        a[j+1] = key;
     }
-};
 
+    auto end = chrono::high_resolution_clock::now();
+    int duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+    sortTimeMap["InsertionSort"] = duration;
+    showSortedNumbers("InsertionSort", a);
+}
 
-// Program główny
+void MySort::myBucketSort() {
+    vector<int> a = tab;
+    auto start = chrono::high_resolution_clock::now();
+
+    int maxVal = *max_element(a.begin(), a.end());
+    int minVal = *min_element(a.begin(), a.end());
+    int bucketCount = max(1, (int)sqrt(n));
+    vector<vector<int>> buckets(bucketCount);
+
+    for (int x : a) {
+        int idx = (int)((long long)(x - minVal) * bucketCount / (maxVal - minVal + 1));
+        buckets[idx].push_back(x);
+    }
+
+    a.clear();
+    for (auto &b : buckets) {
+        sort(b.begin(), b.end());
+        for (int x : b) a.push_back(x);
+    }
+
+    auto end = chrono::high_resolution_clock::now();
+    int duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+    sortTimeMap["BucketSort"] = duration;
+    showSortedNumbers("BucketSort", a);
+}
+
+void MySort::myBogoSort() {
+    vector<int> a = tab;
+    auto start = chrono::high_resolution_clock::now();
+
+    int attempts = 0;
+    while (!isSorted(a) && attempts < 50000) {
+        random_shuffle(a.begin(), a.end());
+        attempts++;
+    }
+
+    auto end = chrono::high_resolution_clock::now();
+    int duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+    sortTimeMap["BogoSort"] = duration;
+    showSortedNumbers("BogoSort", a);
+}
+
 int main() {
-    Ssaki tygrys("Tygrys", 5);
-    Ptak papuga("Papuga", 2);
-    Gad krokodyl("Krokodyl", 7);
-
-    tygrys.opis();
-    tygrys.dzwiek();
-    cout << endl;
-
-    papuga.opis();
-    papuga.dzwiek();
-    cout << endl;
-
-    krokodyl.opis();
-    krokodyl.dzwiek();
-
-    return 0;
+    MySort s;
+    s.getData();
+    s.myBubbleSort();
+    s.mySelectionSort();
+    s.myInsertionSort();
+    s.myBucketSort();
+    s.myBogoSort();
+    s.getSortTimeDuration();
 }
