b7f00d6feac05facd24ac4f09a8673a1ff2ff473
NVM <162157911+HAJTIDev@users.noreply.github.com>
2026-01-19 10:04:14 +0100

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,28 +1,15 @@
 #include <iostream>
 using namespace std;
 
+long long nwd(long long a, long long b) {
+    if (b == 0)
+        return a;
+    return nwd(b, a % b);
+}
+
 int main() {
-    char a, b;
+    long long a, b;
     cin >> a >> b;
-
-    int x, y;
-
-    // pierwszy znak
-    if (a >= '0' && a <= '9')
-        x = a - '0';
-    else
-        x = a - 'A' + 10;
-
-    // drugi znak
-    if (b >= '0' && b <= '9')
-        y = b - '0';
-    else
-        y = b - 'A' + 10;
-
-    int ascii = x * 16 + y;
-
-    cout << ascii << endl;
-    cout << char(ascii) << endl;
-
+    cout << nwd(a, b);
     return 0;
 }
