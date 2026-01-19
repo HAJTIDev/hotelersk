519a492aed63a0f9e5708a2481df8f173dccee2d
NVM <162157911+HAJTIDev@users.noreply.github.com>
2026-01-19 12:19:11 +0100

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,15 +1,63 @@
 #include <iostream>
 using namespace std;
 
-long long nwd(long long a, long long b) {
-    if (b == 0)
-        return a;
-    return nwd(b, a % b);
-}
+class Calculator {
+public:
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    double subtract(double a, double b) {
+        return a - b;
+    }
+
+    double multiply(double a, double b) {
+        return a * b;
+    }
+
+    double divide(double a, double b) {
+        if (b == 0) {
+            cout << "Blad: dzielenie przez zero!" << endl;
+            return 0;
+        }
+        return a / b;
+    }
+};
 
 int main() {
-    long long a, b;
-    cin >> a >> b;
-    cout << nwd(a, b);
+    Calculator calc;
+    double a, b;
+    int choice;
+
+    cout << "Podaj pierwsza liczbe: ";
+    cin >> a;
+    cout << "Podaj druga liczbe: ";
+    cin >> b;
+
+    cout << "\nWybierz operacje:\n";
+    cout << "1. Dodawanie\n";
+    cout << "2. Odejmowanie\n";
+    cout << "3. Mnozenie\n";
+    cout << "4. Dzielenie\n";
+    cout << "Twoj wybor: ";
+    cin >> choice;
+
+    switch (choice) {
+        case 1:
+            cout << "Wynik: " << calc.add(a, b);
+            break;
+        case 2:
+            cout << "Wynik: " << calc.subtract(a, b);
+            break;
+        case 3:
+            cout << "Wynik: " << calc.multiply(a, b);
+            break;
+        case 4:
+            cout << "Wynik: " << calc.divide(a, b);
+            break;
+        default:
+            cout << "Nieprawidlowy wybor!";
+    }
+
     return 0;
 }
