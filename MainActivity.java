3295a463d2ed12f3d10ce3954210f9a812eae312
NVM <162157911+HAJTIDev@users.noreply.github.com>
2026-03-09 11:57:59 +0100

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,9 +1,39 @@
-<androidx.appcompat.widget.SwitchCompat
-    android:id="@+id/switch_theme"
-    android:layout_width="wrap_content"
-    android:layout_height="wrap_content"
-    android:text="Tryb ciemny"
-    android:layout_marginTop="16dp"
-    app:layout_constraintTop_toTopOf="parent"
-    app:layout_constraintStart_toStartOf="parent"
-    app:layout_constraintEnd_toEndOf="parent"/>
+#include <iostream>
+#include <cmath>
+
+class PoleFigury {
+public:
+    // 1. Circle area (a = radius)
+    double pole(double a) {
+        return M_PI * a * a;
+    }
+
+    // 2. Rectangle area
+    double pole(double a, double b) {
+        return a * b;
+    }
+
+    // 3. Trapezoid area
+    double pole(double a, double b, double h) {
+        return (a + b) * h / 2.0;
+    }
+};
+
+int main() {
+    PoleFigury figura;
+
+    double r = 5.0;
+    double a = 4.0, b = 6.0;
+    double a2 = 3.0, b2 = 7.0, h = 4.0;
+
+    std::cout << "Pole kola (r=" << r << "): "
+              << figura.pole(r) << std::endl;
+
+    std::cout << "Pole prostokata (" << a << "x" << b << "): "
+              << figura.pole(a, b) << std::endl;
+
+    std::cout << "Pole trapezu (a=" << a2 << ", b=" << b2 << ", h=" << h << "): "
+              << figura.pole(a2, b2, h) << std::endl;
+
+    return 0;
+}
