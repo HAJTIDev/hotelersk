4a3403280c1ef4c4ba2ad625fce277d297dfc45a
NVM <162157911+HAJTIDev@users.noreply.github.com>
2026-03-24 10:03:52 +0100

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,39 +1,37 @@
 #include <iostream>
-#include <cmath>
+using namespace std;
 
-class PoleFigury {
-public:
-    // 1. Circle area (a = radius)
-    double pole(double a) {
-        return M_PI * a * a;
+void sort3(int *a, int *b, int *c) {
+    int temp;
+
+    if (*a > *b) {
+        temp = *a;
+        *a = *b;
+        *b = temp;
     }
 
-    // 2. Rectangle area
-    double pole(double a, double b) {
-        return a * b;
+    if (*a > *c) {
+        temp = *a;
+        *a = *c;
+        *c = temp;
     }
 
-    // 3. Trapezoid area
-    double pole(double a, double b, double h) {
-        return (a + b) * h / 2.0;
+    if (*b > *c) {
+        temp = *b;
+        *b = *c;
+        *c = temp;
     }
-};
+}
 
 int main() {
-    PoleFigury figura;
+    int x, y, z;
 
-    double r = 5.0;
-    double a = 4.0, b = 6.0;
-    double a2 = 3.0, b2 = 7.0, h = 4.0;
+    cout << "Podaj trzy liczby: ";
+    cin >> x >> y >> z;
 
-    std::cout << "Pole kola (r=" << r << "): "
-              << figura.pole(r) << std::endl;
+    sort3(&x, &y, &z);
 
-    std::cout << "Pole prostokata (" << a << "x" << b << "): "
-              << figura.pole(a, b) << std::endl;
-
-    std::cout << "Pole trapezu (a=" << a2 << ", b=" << b2 << ", h=" << h << "): "
-              << figura.pole(a2, b2, h) << std::endl;
+    cout << "Posortowane: " << x << " " << y << " " << z << endl;
 
     return 0;
 }
