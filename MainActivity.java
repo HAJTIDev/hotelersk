dea342055156b02245cf1ace713da3f2344341c6
NVM <162157911+HAJTIDev@users.noreply.github.com>
2026-05-11 10:12:10 +0200

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,60 +1,25 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 
 using namespace std;
-
-char getGender(const string& pesel) {
-    int digit = pesel[9] - '0';
-
-    if (digit % 2 == 0)
-        return 'K';
-    else
-        return 'M';
+template <typename T>
+T maks(T a, T b)
+{
+    return (a > b) ? a : b;
+}
+template <>
+const char* maks<const char*>(const char* a, const char* b)
+{
+    return (strcmp(a, b) > 0) ? a : b;
 }
 
-bool isValidPesel(const string& pesel) {
-    int weights[10] = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3};
-    int sum = 0;
+int main()
+{
+    cout << maks(5, 7) << endl;
 
-    for (int i = 0; i < 10; i++) {
-        int digit = pesel[i] - '0';
-        sum += digit * weights[i];
-    }
+    cout << maks(3.14, 2.71) << endl;
 
-    int M = sum % 10;
-    int R;
-
-    if (M == 0)
-        R = 0;
-    else
-        R = 10 - M;
-
-    int controlDigit = pesel[10] - '0';
-
-    return R == controlDigit;
-}
-
-int main() {
-    string pesel;
-
-    cout << "Podaj numer PESEL: ";
-    cin >> pesel;
-
-    if (pesel.length() != 11) {
-        cout << "Nieprawidlowa dlugosc numeru PESEL!" << endl;
-        return 1;
-    }
-
-    char gender = getGender(pesel);
-    if (gender == 'K')
-        cout << "Plec: Kobieta" << endl;
-    else
-        cout << "Plec: Mezczyzna" << endl;
-
-    if (isValidPesel(pesel))
-        cout << "PESEL jest poprawny." << endl;
-    else
-        cout << "PESEL jest NIEPOPRAWNY." << endl;
+    cout << maks("ALA", "Zosia") << endl;
 
     return 0;
 }
